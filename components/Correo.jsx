9ff8c2bf2c89f6c8750f7bb3ad7826c8765acfd5
Pedro <96238562+PedroVader@users.noreply.github.com>
2022-02-11 11:17:57 +0100

    Update Correo.jsx

--- components/Correo.jsx
+++ components/Correo.jsx
@@ -6,7 +6,7 @@ const Correo = () => {
   <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
       <div className="text-xl mb-5 font-semibold border-b pb-4">
           <h2>También puedes contactar con nosotros vía email a
-              <p> <a className="underline" href="mailto:info.tribu8@gmail.com"> info.tribu8(arroba)gmail.com ✉️ </a> </p>
+              <a className="underline" href="mailto:info.tribu8@gmail.com"> info.tribu8(arroba)gmail.com ✉️ </a>
           </h2>
       </div>
   </div>
